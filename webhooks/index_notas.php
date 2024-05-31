4364180ea19ad4c2aa026e6784ee5f586af17049
brandon <brandon.djpon000@gmail.com>
2024-05-31 12:22:18 -0600

    ADD: DB al webhook, pruebas de 'cambios'

---
+++ webhooks/index_notas.php
@@ -0,0 +1,43 @@
+<?php
+include 'model.php';
+$model = new Model();
+
+// $archivo = 'webhoo.txt';
+
+// $json = $model->ToJson(file_get_contents('webhoo.txt'));
+// $leads = $json['leads']['update'][0];
+
+// echo '<br> idkommo: '.$leads['id'];
+// echo '<br> lead_nombre: '.$leads['name'];
+// echo '<br> pipeline: '.$leads['pipeline_id'];
+// echo '<br> etapa: '.$leads['status_id'];
+// echo '<br> id_responsable: '.$leads['responsible_user_id'];
+// echo '<br> fecha: '.$model->getDate($leads['date_create']);
+
+
+// echo '-----------------------------------------<br>';
+// echo '<br> fecha_asignacion: '.$model->getValueFromItem($leads['custom_fields'], 'name','Fuente')['values'][0]['value'];
+
+// echo '<br> fuente: '.
+//     $model->getDate(
+//     $model->getValueFromItem($leads['custom_fields'], 'name','Fecha de Asignación')['values'][0]
+//     );
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $json = $model->ToJson(file_get_contents("php://input"));
+    // $leads = $json['leads']['update'][0];
+    // $model->
+    // insertCambios(
+    //     // $idkommo,$lead_nombre,$pipeline,
+    //     $leads['id'],$leads['name'],$leads['pipeline_id'],
+    //     // $etapa,$id_responsable,$fecha,
+    //     $leads['status_id'],$leads['responsible_user_id'],$model->getDate($leads['date_create']),
+    //     // $fuente,$fecha_asignacion
+    //     $model->getValueFromItem($leads['custom_fields'], 'name','Fuente')['values'][0]['value'],
+    //     $model->getDate($model->getValueFromItem($leads['custom_fields'], 'name','Fecha de Asignación')['values'][0])
+    // );
+}
+
+
+?>
--- webhooks/model.php
+++ webhooks/model.php
@@ -1,38 +1,129 @@
 <?php
-include '../database.php';
+include 'database.php';
 class Model
 {
-    public function selectBy($var) {
+    function ToJson($text_url)
+    {
+        // RETIRAR CARACTERES URL
+        $text = urldecode($text_url);
+        $text = str_replace("&", "\n", $text);
+        // // SEGMENTAR EL TEXTO EN LÍNEAS
+        $lines = explode("\n", $text);
+        $data = array();
+        foreach ($lines as $l) {
+            // DIVIDIR LA LINEA - > CLAVE VALOR
+            list($key, $value) = explode("=", $l, 2);
+            $key = trim($key);
+            $value = trim($value);
+            $value = str_replace(array('[', ']'), '', $value);
+
+            // CONVERTIR LA CLAVE
+            $part = explode('[', $key);
+            $actual = &$data;
+
+            // RECORRER CADA PARTE DE LA CLAVE
+            foreach ($part as $p) {
+                $p = rtrim($p, ']');
+
+                // CREAR EL ARREGLO ASOCIATIVO SI NO EXISTE
+                if (!isset($actual[$p])) {
+                    $actual[$p] = array();
+                }
+
+                // CAMBIAR EL PUNTERO AL SIGUIENTE
+                $actual = &$actual[$p];
+            }
+
+            // ASIGNAR EL VALOR A LA CLAVE FINAL
+            $actual = $value;
+        }
+        return $data;
+    }
+    function getDate($time){
+        return date('Y-m-d H:i:s', $time);
+    }
+    function getValueFromItem($list,$item_key,$name){
+        foreach ($list as $l) {
+            if ($l[$item_key] == $name) {
+                return $l;
+            }
+        }
+    }
+    // -------------------------------------------------------------
+    function insertCambios(
+        $idkommo,$lead_nombre,$pipeline,
+        $etapa,$id_responsable,$fecha,
+        $fuente,$fecha_asignacion
+    ){
         $mysqli = conectarDB();
         if ($mysqli->connect_error) {die("Error de conexión: " . $mysqli->connect_error);}
-        $sql = "SELECT
-            GROUP_CONCAT(concat(p.user_name,' ')) 'names',
-            COUNT(p.ticket) 'nticket',
-            p.email,
-            SUM(p.score) AS 'max_score'
-            
-            FROM tbl_puntajes p
-            WHERE WEEK(p.date) = WEEK(CURDATE()) - '".$var."'
-            
-            GROUP BY p.email
-            ORDER BY max_score DESC 
-        ";
-        $datos = array();
 
-        $result = $mysqli->query($sql);
+        $sql = "INSERT INTO leads_kommo_cambios (
+            `idkommo`,`lead_nombre`,`pipeline`,
+            `etapa`,`id_responsable`,`fecha`,
+            `fuente`,`fecha_asignacion`
+        )VALUES(
+            ".$idkommo.",
+            '".$lead_nombre."',
+            ".$pipeline.",
 
-        if ($result && $result->num_rows == 0) {
-            $mysqli->close();
+            ".$etapa.",
+            ".$id_responsable.",
+            '".$fecha."',
 
-            return json_decode('[]');
-        }else{
-            while ($row = $result->fetch_assoc()) {
-            $datos[] = $row;
-        }
+            '".$fuente."',
+            '".$fecha_asignacion."'
+        )";
+
+        if ($mysqli->query($sql) != 1) {echo '<script>console.log('.$mysqli->error.')</script>';}
         $mysqli->close();
-        return $datos;
-        }
-        
     }
+    function insertNotas(
+        $idkommo,$lead_nombre,$actividad,
+        $id_responsable,$fecha,$fecha_asignacion
+    ){
+        $mysqli = conectarDB();
+        if ($mysqli->connect_error) {die("Error de conexión: " . $mysqli->connect_error);}
+
+        $sql = "INSERT INTO leads_kommo_notas (
+            `idkommo`,`lead_nombre`,`actividad`,
+            `id_responsable`,`fecha`,`fecha_asignacion`
+        )VALUES(
+            ".$idkommo.",
+            '".$lead_nombre."',
+            '".$actividad."',
+            
+            ".$id_responsable.",
+            '".$fecha."',
+            '".$fecha_asignacion."'
+        )";
+
+        if ($mysqli->query($sql) != 1) {echo '<script>console.log('.$mysqli->error.')</script>';}
+        $mysqli->close();
+    }
+    function insertTareas(
+        $idkommo,$lead_nombre,$actividad,
+        $id_responsable,$fecha,$fecha_asignacion
+    ){
+        $mysqli = conectarDB();
+        if ($mysqli->connect_error) {die("Error de conexión: " . $mysqli->connect_error);}
+
+        $sql = "INSERT INTO leads_kommo_tareas (
+            `idkommo`,`lead_nombre`,`actividad`,
+	        `id_responsable`,`fecha`,`fecha_asignacion`
+        )VALUES(
+            ".$idkommo.",
+            '".$lead_nombre."',
+            '".$actividad."',
+
+            ".$id_responsable.",
+            '".$fecha.",'
+            '".$fecha_asignacion."'
+        )";
+
+        if ($mysqli->query($sql) != 1) {echo '<script>console.log('.$mysqli->error.')</script>';}
+        $mysqli->close();
+    }
+
 }
 ?>
